6f7034070b8d81726687e05b731ccc5b36a98963
Andres <andresdavidagudelo01@gmail.com>
2025-02-18 11:19:15 -0500

    added date_creation and class notebook with self.notes

--- app/notebook.py
+++ app/notebook.py
@@ -15,3 +15,9 @@ class Note:
     def add_tag(self,tag:str):
         if tag not in self.tags:
             self.tags.append(tag)
+    def date_creation(self):
+        return f"{self.creation_date}-{self.title}"
+
+class Notebook:
+    def __init__(self):
+        self.notes:list[Note]=[]
