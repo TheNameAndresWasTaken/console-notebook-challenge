b6ae3b7132a9c2a1f43fd78ab2e2f759f13923a3
Andres <andresdavidagudelo01@gmail.com>
2025-02-18 10:54:48 -0500

    Class note done

--- app/notebook.py
+++ app/notebook.py
@@ -1,1 +1,14 @@
-# TODO: Agrega el código de las clases del modelo aquí. Borra este comentario al terminar.
+from datetime import datetime
+
+class Note:
+    HIGH:str="HIGH"
+    MEDIUM:str="MEDIUM"
+    LOW:str="LOW"
+    def __init__(self, code: str,title: str, text:str, improtance:str,creation_date: datetime, tags: list[str]):
+        self.code: str=code
+        self.title: str = title
+        self.text: str = text
+        self.importance: str = improtance
+        self.creation_date:datetime =datetime.now()
+
+
